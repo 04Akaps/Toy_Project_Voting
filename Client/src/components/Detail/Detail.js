22bb4c56a7d8c5bc83154231a745d073c6d8209c
유호진 <sdl155@jbnu.ac.kr>
2022-03-10 16:03:34 +0900

    caver-js연동 - 1

--- Client/src/components/Detail/Detail.js
+++ Client/src/components/Detail/Detail.js
@@ -1,14 +1,66 @@
-
-import React, { useState } from "react";
-import "./Detail.scss"
+import React, { useEffect, useState } from "react";
+import { Voting_Contract } from "../Contract/Caver";
+import { Unix_timestamp } from "../function";
+import "./Detail.scss";
 
 function Detail() {
-    const idx = useState(window.location.pathname.split(":")[1]);
-    
+  const [Data, SetData] = useState(false);
+  const idx = window.location.pathname.split(":")[1];
 
-    return <div className="Detail_App">hoijn</div>
-    
+  useEffect(async () => {
+    Voting_Contract().then(async (method) => {
+      const result = await method.view_Proposal(idx).call();
+      SetData(result);
+    });
+  }, []);
+  console.log(Data);
+  return (
+    <div className="Detail_App">
+      {Data ? (
+        <>
+          <div className="Detail_upper_Container">
+            <div className="Upper_Container_div">
+              <h2 className="Upper_Conatiner_h2">Detail_Page</h2>
+              <div className="Upper_Conatiner_under_div">
+                You Can See a Detail Information
+              </div>
+              <span className="Upper_Container_span">
+                주최자 :{Data.Starter_user}
+              </span>
+              <span className="Upper_Container_span">
+                투표수 : {Data.count}
+              </span>
+              <span className="Upper_Container_span">
+                끝나는 시간 :{Unix_timestamp(Data.end_time)}
+              </span>
+            </div>
+          </div>
+          <div className="Detail_bottom_Container">
+            <div className="Detail_bottom_Content_div">
+              <div className="Detail_bottom_Conetnt_upper_div">
+                <h2 className="Detail_bottom_Content_h2">Content</h2>
+                <div className="Detail_bottom_Conetnt_Content">
+                  {Data.content}
+                </div>
+              </div>
+              <div className="Detail_bottom_Conetnt_bottom_div">
+                <h2 className="Detail_bottom_Content_h2">Voting_User</h2>
+                <div className="Deatil_bottom_Conent_Voting_User">
+                  {Data.aggred_User.length === 0
+                    ? "null"
+                    : Data.aggred_User.map((a) => {
+                        return <p>a</p>;
+                      })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        "Loading"
+      )}
+    </div>
+  );
 }
-
 
 export default Detail;
--- README.md
+++ README.md
@@ -15,8 +15,12 @@ Back_end : TypeScript
 
 - `main`
 
-03.09 - 2 : Solidity코드 얼추 마무리, Truffle & HDwallet?? 이쪽 버전상의 문제인지 Truffle부분으로 배포가 안되어서 일단 Remix에서 배포후 값을 따옴
+  03.09 - 2 : Solidity코드 얼추 마무리, Truffle & HDwallet?? 이쪽 버전상의 문제인지 Truffle부분으로 배포가 안되어서 일단 Remix에서 배포후 값을 따옴
 
 - `Solidity`
+
+  03.10 - 1 : Solidity코드 살짝 수정 및 Caver-js와 React를 연동중, Detail, mainPage 정도 작성이 완료
+
+- `main`
 
 velog - https://velog.io/@jjimgo/Voting-사이트
