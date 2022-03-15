f89d76ac88b95247a6b8a6b71d8fe0a7c43839af
유호진 <83758624+jjimgo@users.noreply.github.com>
2022-03-15 22:46:22 +0900

    Update Caver_function.js

--- Client/src/components/Caver_function.js
+++ Client/src/components/Caver_function.js
@@ -24,6 +24,12 @@ export const check_time_for_delete = async () => {
       if (!check_time(data[i].end_time)) {
         let delete_data = method.done_Proposal(data[i].id).encodeABI();
         let tx = await make_Tx(delete_data);
+        
+        /*
+          사실 이 부분은 tx에서 값을 따오지 않고 바로 data[i]에서 값을 따와도 됩니다.
+
+          이렇게 작성한 이유는 제가 이런식으로 작성을 하였고 후에 다시 생각을 해보니 이런 부분이 수정되었으면 좋겠다는 생각이 들어서 기록하기 위해서 이런방향으로 작성을 하였습니다.
+        */
 
         const { rawTransaction: senderRawTransaction } =
           await caver.klay.accounts.signTransaction(
